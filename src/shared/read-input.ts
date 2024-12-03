bf8e1547d193d02113fb061f3d608905421972e9
keann <code@keann.dev>
2024-12-03 15:11:15 +0700

    chore: workspace settings

--- src/shared/read-input.ts
+++ src/shared/read-input.ts
@@ -1,8 +1,16 @@
 import path from 'node:path'
 
-export const readInputStrings = async (inputFileName = 'input.txt') => {
-	const inputPath = path.join(import.meta.dir, inputFileName)
+const INPUT_FILE_NAME = 'input.txt'
+
+export const readInputFile = async () => {
+	const inputPath = path.join(process.env.CWD ?? '', INPUT_FILE_NAME)
 	const file = await Bun.file(inputPath).text()
+
+	return file
+}
+
+export const readInputStrings = async () => {
+	const file = await readInputFile()
 	const strings = file.split('\n')
 
 	return strings
